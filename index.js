717925d2c43ec1c3243fbeebe75ffc74098e53ae
Craig Wills <2634+cdwills@users.noreply.github.com>
2019-02-05 18:29:52 -0600

    better setup for jwksOptionsDefaults

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jwks     = require('jwks-rsa')
 const jwt      = require('jsonwebtoken')
 
 const {
-  applyTo: thrush, compose, composeP, curryN, dissoc, merge,
+  applyTo: thrush, compose, composeP, curryN, omit, merge,
   partialRight, prop, replace
 } = require('ramda')
 
@@ -12,19 +12,14 @@ const { promisify, rename, tapP } = require('@articulate/funky')
 
 const wellKnown = '/.well-known/openid-configuration'
 
-const jwksSettings = {
-  cache: true,
-  rateLimit: true
-}
-
 const bindFunction = client =>
   promisify(client.getSigningKey, client)
 
-const buildClient = url =>
+const buildClient = (jwksOpts, url) =>
   gimme({ url })
     .then(prop('body'))
     .then(rename('jwks_uri', 'jwksUri'))
-    .then(compose(jwks, merge(jwksSettings)))
+    .then(compose(jwks, merge(jwksOpts)))
     .then(bindFunction)
 
 const chooseKey = key =>
@@ -41,9 +36,13 @@ const stripBearer =
 const unauthorized = err =>
   Promise.reject(Boom.wrap(err, 401))
 
-const factory = opts => {
+const jwksOptsDefaults = { jwks: { cache: true, rateLimit: true } }
+
+const factory = (options = {}) => {
   const clients    = {}
-  const verifyOpts = dissoc('issWhitelist', opts)
+  const opts = merge(options, jwksOptsDefaults)
+  const verifyOpts = omit([ 'issWhitelist', 'jwks' ], opts)
+  const jwksOpts   = prop('jwks', opts)
 
   const cacheClient = iss => client =>
     clients[iss] = client
@@ -55,7 +54,7 @@ const factory = opts => {
   const getSigningKey = ({ header: { kid }, payload: { iss } }) =>
     clients[iss]
       ? clients[iss](kid)
-      : buildClient(iss.replace(/\/$/, '') + wellKnown)
+      : buildClient(jwksOpts, iss.replace(/\/$/, '') + wellKnown)
         .then(cacheClient(iss))
         .then(thrush(kid))
 
