6a5be00ee83afcdc584ec2ea31da14f3d9dafa35
Tyler O'Donnell <tyler@tylerodonnell.com>
2019-03-01 16:05:02 -0500

    Differentiate forbidden and unauthorized errors

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const jwks     = require('jwks-rsa')
 const jwt      = require('jsonwebtoken')
 
 const {
-  applyTo: thrush, compose, composeP, curryN, merge,
-  mergeDeepRight, partialRight, prop, replace
+  applyTo: thrush, compose, composeP, curryN, isNil, ifElse, merge,
+  mergeDeepRight, partialRight, pathEq, prop, replace, when,
 } = require('ramda')
 
 const { promisify, rename, tapP } = require('@articulate/funky')
@@ -30,11 +30,23 @@ const decode = partialRight(jwt.decode, [{ complete: true }])
 const enforce = token =>
   token || Promise.reject(Boom.unauthorized('null token not allowed'))
 
+const forbidden = err =>
+  Promise.reject(Boom.forbidden(err))
+
+const isExpired =
+  pathEq(['name'], 'TokenExpiredError')
+
 const stripBearer =
   replace(/^Bearer /i, '')
 
+const throwIfNull =
+  when(isNil, () => Promise.reject(new Error('invalid token')))
+
 const unauthorized = err =>
   Promise.reject(Boom.unauthorized(err))
+
+const deny =
+  ifElse(isExpired, forbidden, unauthorized)
 
 const jwksOptsDefaults = { jwks: { cache: true, rateLimit: true } }
 
@@ -65,11 +77,12 @@ const factory = options => {
   const authentic = token =>
     Promise.resolve(token)
       .then(decode)
+      .then(throwIfNull)
       .then(tapP(checkIss))
       .then(getSigningKey)
       .then(chooseKey)
       .then(verify(token))
-      .catch(unauthorized)
+      .catch(deny)
 
   return composeP(authentic, stripBearer, tapP(enforce))
 }
